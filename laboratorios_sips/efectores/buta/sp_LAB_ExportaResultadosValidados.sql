69d548a302838e6f6b91a4946f3b3f9686375835
Orlando Brea <orlando.brea@gmail.com>
2021-04-13 09:30:46 -0300

    chore: agrego buta actualizado

--- laboratorios_sips/efectores/buta/sp_LAB_ExportaResultadosValidados.sql
+++ laboratorios_sips/efectores/buta/sp_LAB_ExportaResultadosValidados.sql
@@ -1,26 +1,29 @@
-USE [SIPSButa]
+USE [SIntegralH]
 GO
-/****** Object:  StoredProcedure [dbo].[LAB_ExportaResultadosValidados]    Script Date: 09/11/2020 11:04:24 ******/
+/****** Object:  StoredProcedure [dbo].[LAB_ExportaResultadosValidados]    Script Date: 13/04/2021 9:25:06 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[LAB_ExportaResultadosValidados]
-WITH EXECUTE AS CALLER
-AS
-BEGIN
+
 /*
 Update: 2016-09-26 - Julio: Agrego la columna baja en LAB_Temp_ResultadoEncabezado y en el select de los protocolos
 */
+ALTER PROCEDURE [dbo].[LAB_ExportaResultadosValidados]
+WITH EXECUTE AS CALLER
+AS
+BEGIN
+
 TRUNCATE TABLE LAB_Temp_ResultadoEncabezado
 TRUNCATE TABLE LAB_Temp_ResultadoDetalle
 
 declare  @dias int 
-set @dias=3
+set @dias=7
 
+--exec [LAB_ExportaResultadosValidados]
 create table #TableFinal (idProtocolo int)
 
---select GETDATE()-38
+--select GETDATE()
 
 insert into #TableFinal 
 --select distinct idProtocolo from lab_protocolo where numero in (13297,16340,16354,26534,34528,38809,34529)
@@ -29,16 +32,15 @@ SELECT DISTINCT P.idProtocolo FROM dbo.LAB_DetalleProtocolo as  DP
 inner join LAB_Protocolo as P on P.idProtocolo=DP.idProtocolo
 WHERE 
 (P.baja = 0) and (P.idTipoServicio<4) 
-and ((CONVERT(varchar, fechaValida, 112) >=  CONVERT(varchar,GETDATE()-@dias, 112)
-or  CONVERT(varchar, fechaValidaObservacion, 112) >= CONVERT(varchar,GETDATE()-@dias, 112)))
+and (CONVERT(varchar(8), fechaValida, 112) >=  CONVERT(varchar(8),GETDATE()-@dias, 112)
+or  CONVERT(varchar(8), fechaValidaObservacion, 112) >= CONVERT(varchar(8),GETDATE()-@dias, 112))
 
---and ((CONVERT(varchar, fechaValida, 112) between '20160720' and '20160727' ) or  CONVERT(varchar, fechaValidaObservacion, 112) between '20160720' and '20160727' )
+--and ((CONVERT(varchar, fechaValida, 112) between '20160421' and '20160421' ) or  CONVERT(varchar, fechaValidaObservacion, 112) between '20160421' and '20160421' )
 
 --and ((CONVERT(varchar, fechaValida, 112) <> '19000101' ) or  CONVERT(varchar, fechaValidaObservacion, 112) <> '19000101')
---and P.idPaciente = 185070
+--and P.idPaciente=1392686
 
 --and ( CONVERT(varchar(6), fechaValida, 112) = '201507' or CONVERT(varchar(6), fechaValidaObservacion, 112) = '201507' )
-
 
 INSERT INTO LAB_Temp_ResultadoEncabezado         
 SELECT DISTINCT P.idProtocolo, P.idEfector, Pac.apellido, Pac.nombre, P.edad, 
@@ -47,8 +49,8 @@ SELECT DISTINCT P.idProtocolo, P.idEfector, Pac.apellido, Pac.nombre, P.edad,
              Pac.referencia  AS domicilio, Pac.historiaClinica AS HC, Pri.nombre AS prioridad, O.nombre AS origen, dbo.NumeroProtocolo(P.idProtocolo) AS numero, 
              dbo.ImprimeHiv(P.idProtocolo) AS hiv, UPPER(Prof.solicitante) AS solicitante, SS.nombre AS sector, P.sala, P.cama, 
 			 CASE WHEN PD.iddiagnostico IS NULL THEN '' ELSE 'E' END AS embarazo, ES.nombre AS EfectorSolicitante,
-			 M.nombre as tipoMuestra ,null as idSolicitudScreening, null as fechaRecibeScreening,
-			 P.observacionesResultados, P.baja
+			 null as idSolicitudScreening, null as fechaRecibeScreening,
+			 P.observacionesResultados, M.nombre as tipoMuestra, P.baja
 FROM         dbo.LAB_Protocolo AS P INNER JOIN
                       dbo.Sys_Paciente AS Pac ON P.idPaciente = Pac.idPaciente INNER JOIN
                       dbo.LAB_Origen AS O ON P.idOrigen = O.idOrigen INNER JOIN
@@ -80,10 +82,10 @@ CASE WHEN I1.idCategoria = 1 THEN I1.descripcion ELSE CASE WHEN I.idcategoria = 
                       CASE WHEN I1.idCategoria = 1 THEN 'Si' ELSE 'No' END AS esTitulo, CASE WHEN I.idEfectorDerivacion <> i.idefector THEN ED.nombre ELSE '' END AS derivado, 
                       DP.unidadMedida AS unidad, dbo.ImprimeHiv(P.idProtocolo) AS hiv, DP.metodo, DP.valorReferencia, DP.idDetalleProtocolo AS orden1, DP.trajoMuestra AS muestra, 
                       CASE WHEN DP.trajomuestra = 'No' THEN 1 ELSE CASE WHEN I.idEfectorDerivacion <> i.idefector THEN 1 ELSE conResultado END END AS conresultado, 
-                      CASE WHEN I1.idTipoResultado <> 1 THEN DP.resultadoCar ELSE CASE I1.formatoDecimal WHEN 0 THEN CAST(CAST(resultadonum AS  decimal(18, 0)) AS varchar(50)) 
+                    case when I.idEfectorDerivacion <> i.idefector then 'Derivado ' + ED.nombre else  CASE WHEN I1.idTipoResultado <> 1 THEN DP.resultadoCar ELSE CASE I1.formatoDecimal WHEN 0 THEN CAST(CAST(resultadonum AS  decimal(18, 0)) AS varchar(50)) 
                       WHEN 1 THEN CAST(CAST(resultadonum AS decimal(18, 1)) AS varchar(50)) WHEN 2 THEN CAST(CAST(resultadonum AS decimal(18, 2)) AS varchar(50)) 
                       WHEN 3 THEN CAST(CAST(resultadonum AS decimal(18, 3)) AS varchar(50)) WHEN 4 THEN CAST(CAST(resultadonum AS decimal(18, 4)) AS varchar(50)) 
-                      END END AS resultado, I1.codigo AS codigo2,  U.firmaValidacion as profesional_val
+                      END END end AS resultado, I1.codigo AS codigo2,  U.firmaValidacion as profesional_val
 FROM         LAB_Temp_ResultadoEncabezado AS P INNER JOIN
                       LAB_DetalleProtocolo AS DP ON DP.idProtocolo = P.idProtocolo INNER JOIN
                       LAB_Item AS I ON DP.idItem = I.idItem AND DP.idEfector = I.idEfector INNER JOIN
